c7db8e5e81e27867c12f1e96218c6d53ea27c75c
Marie-Ève <marieeve.malette@gmail.com>
2019-03-25 22:17:46 -0400

    third version

--- question5.3.py
+++ question5.3.py
@@ -19,131 +19,72 @@ np = numpy
 
 # NOTE ==============================================
 # This is where your models are imported
-from models import RNN, GRU 
-from models import make_model as TRANSFORMER
-from ptblm import run_epoch2, ptb_raw_data, run_epoch
+from models import RNN, GRU
+import math
 
+def _read_words(filename):
+    with open(filename, "r") as f:
+      return f.read().replace("\n", "<eos>").split()
 
+def _build_vocab(filename):
+    data = _read_words(filename)
 
-path=os.path.join(os.getcwd(),'best_model','Transformer 4.1', 'best_params.pt')
+    counter = collections.Counter(data)
+    count_pairs = sorted(counter.items(), key=lambda x: (-x[1], x[0]))
 
+    words, _ = list(zip(*count_pairs))
+    word_to_id = dict(zip(words, range(len(words))))
+    id_to_word = dict((v, k) for k, v in word_to_id.items())
 
-##############################################################################
-#
-# ARG PARSING AND EXPERIMENT SETUP
-#
-##############################################################################
-
-parser = argparse.ArgumentParser(description='PyTorch Penn Treebank Language Modeling')
-
-# Arguments you may need to set to run different experiments in 4.1 & 4.2.
-parser.add_argument('--data', type=str, default='data',
-                    help='location of the data corpus. We suggest you change the default\
-                    here, rather than passing as an argument, to avoid long file paths.')
-parser.add_argument('--model', type=str, default='RNN',
-                    help='type of recurrent net (RNN, GRU, TRANSFORMER)')
-parser.add_argument('--optimizer', type=str, default='ADAM',
-                    help='optimization algo to use; SGD, SGD_LR_SCHEDULE, ADAM')
-parser.add_argument('--seq_len', type=int, default=35,
-                    help='number of timesteps over which BPTT is performed')
-parser.add_argument('--batch_size', type=int, default=20,
-                    help='size of one minibatch')
-parser.add_argument('--initial_lr', type=float, default=0.0001,
-                    help='initial learning rate')
-parser.add_argument('--hidden_size', type=int, default=1500,
-                    help='size of hidden layers. IMPORTANT: for the transformer\
-                    this must be a multiple of 16.')
-parser.add_argument('--save_best', action='store_true',
-                    help='save the model for the best validation performance')
-parser.add_argument('--num_layers', type=int, default=2,
-                    help='number of hidden layers in RNN/GRU, or number of transformer blocks in TRANSFORMER')
-
-# Other hyperparameters you may want to tune in your exploration
-parser.add_argument('--emb_size', type=int, default=200,
-                    help='size of word embeddings')
-parser.add_argument('--num_epochs', type=int, default=40,
-                    help='number of epochs to stop after')
-parser.add_argument('--dp_keep_prob', type=float, default=0.35,
-                    help='dropout *keep* probability. drop_prob = 1-dp_keep_prob \
-                    (dp_keep_prob=1 means no dropout)')
-
-# Arguments that you may want to make use of / implement more code for
-parser.add_argument('--debug', action='store_true') 
-parser.add_argument('--save_dir', type=str, default='',
-                    help='path to save the experimental config, logs, model \
-                    This is automatically generated based on the command line \
-                    arguments you pass and only needs to be set if you want a \
-                    custom dir name')
-parser.add_argument('--evaluate', action='store_true',
-                    help="use this flag to run on the test set. Only do this \
-                    ONCE for each model setting, and only after you've \
-                    completed ALL hyperparameter tuning on the validation set.\
-                    Note we are not requiring you to do this.")
-
-# DO NOT CHANGE THIS (setting the random seed makes experiments deterministic, 
-# which helps for reproducibility)
-parser.add_argument('--seed', type=int, default=1111,
-                    help='random seed')
-
-args = parser.parse_args()
-argsdict = args.__dict__
-argsdict['code_file'] = sys.argv[0]
-
-# Use the model, optimizer, and the flags passed to the script to make the 
-# name for the experimental dir
-print("\n########## Setting Up Experiment ######################")
-flags = [flag.lstrip('--').replace('/', '').replace('\\', '') for flag in sys.argv[1:]]
-experiment_path = os.path.join(args.save_dir+'_'.join([argsdict['model'],
-                                         argsdict['optimizer']] 
-                                         + flags))
-
-# LOAD DATA
-args.data=os.path.join(os.getcwd(),'data')
-print('Loading data from '+args.data)
-raw_data = ptb_raw_data(data_path=args.data)
-train_data, valid_data, test_data, word_to_id, id_2_word = raw_data
-vocab_size = len(word_to_id)
-print('  vocabulary size: {}'.format(vocab_size))
-
-# Increment a counter so that previous results with the same args will not
-# be overwritten. Comment out the next four lines if you only want to keep
-# the most recent results.
-i = 0
-while os.path.exists(experiment_path + "_" + str(i)):
-    i += 1
-experiment_path = experiment_path + "_" + str(i)
-
-# Creates an experimental directory and dumps all the args to a text file
-os.mkdir(experiment_path)
-print ("\nPutting log in %s"%experiment_path)
-argsdict['save_dir'] = experiment_path
-with open (os.path.join(experiment_path,'exp_config.txt'), 'w') as f:
-    for key in sorted(argsdict):
-        f.write(key+'    '+str(argsdict[key])+'\n')
+    return word_to_id, id_to_word
 
 # Set the random seed manually for reproducibility.
-torch.manual_seed(args.seed)
+torch.manual_seed(1111)
 
-# Use the GPU if you have one
-if torch.cuda.is_available():
-    print("Using the GPU")
-    device = torch.device("cuda") 
-else:
-    print("WARNING: You are about to run on cpu, and this will likely run out \
-      of memory. \n You can try setting batch_size=1 to reduce memory usage")
-    device = torch.device("cpu")
+device = torch.device("cpu")
 
-model = RNN(emb_size=args.emb_size, hidden_size=args.hidden_size, 
-                seq_len=args.seq_len, batch_size=args.batch_size,
-                vocab_size=vocab_size, num_layers=args.num_layers, 
-                dp_keep_prob=args.dp_keep_prob).cuda()
+seq_len= 70
+num_seq = 20
 
-path=os.path.join(os.getcwd(),'best_model','RNN 4.1', 'best_params.pt')
-model.load_state_dict(torch.load(path))
+#Prediction for the RNN
+model = RNN(emb_size=200, hidden_size=1500,
+            seq_len=35, batch_size=20,
+            vocab_size=10000, num_layers=2,
+            dp_keep_prob=0.35).cuda()
 
+params_path = os.path.join(os.getcwd(), 'RNN 4.1', 'best_params.pt')
+model.load_state_dict(torch.load(params_path))
+input = 0 * torch.ones(num_seq, dtype=torch.long).cuda()
+hidden = 1.5 * torch.rand(2, num_seq, 1500).cuda()
+gen_seq = model.generate(input, hidden, seq_len).cpu().data.numpy()
 
-input=torch.zeros(2, dtype=torch.long).cuda()
+data_path = os.path.join(os.getcwd(), 'data', 'ptb.train.txt')
+word_2_id, id_2_word = _build_vocab(data_path)
+sentences = ['' for _ in range(num_seq)]
+print('####################################### RNN Sentences #######################################')
+for i in range(num_seq):
+    print(str(i) + ': ')
+    for j in range(seq_len+1):
+        sentences[i] += id_2_word[gen_seq[j, i]] + ' '
+    sentences[i] += '\n'
+    print(sentences[i])
 
+#Prediction for the GRU
+model = GRU(emb_size=200, hidden_size=1500,
+            seq_len=35, batch_size=20,
+            vocab_size=10000, num_layers=2,
+            dp_keep_prob=0.35).cuda()
 
-hidden=torch.zeros(2, 2, 1500).cuda()
-model.generate(input,hidden,35)
+params_path = os.path.join(os.getcwd(), 'GRU 4.1', 'best_params.pt')
+model.load_state_dict(torch.load(params_path))
+input = 0 * torch.ones(num_seq, dtype=torch.long).cuda()
+hidden = 1.75 * torch.rand(2, num_seq, 1500).cuda()
+gen_seq = model.generate(input, hidden, seq_len).cpu().data.numpy()
+sentences = ['' for _ in range(num_seq)]
+print('####################################### GRU Sentences #######################################')
+for i in range(num_seq):
+    print(str(i) + ': ')
+    for j in range(seq_len+1):
+        sentences[i] += id_2_word[gen_seq[j, i]] + ' '
+    sentences[i] += '\n'
+    print(sentences[i])
